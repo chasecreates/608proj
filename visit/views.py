4947fb2c420d2adb9b90ce4bedf068ac433d9a37
chasebyinfinity <chasebyinfinity@gmail.com>
2019-05-07 02:24:21 -0400

    yah

--- visit/views.py
+++ visit/views.py
@@ -28,11 +28,22 @@ def login():
         if(str(user[4]) == request.form['student_id']):
             session['logged_in'] = True
             session['kerberos'] = request.form['kerberos']
-            return redirect(url_for('index'))
+            return redirect(url_for('dashboard'))
         else:
             return 'Incorrect information.'
 
     return render_template('login.html')
+@app.route('/dashboard')
+@login_required
+def dashboard():
+    guest_list = []
+    your_kerberos = session['kerberos']
+    get_guests_of_user(your_kerberos, guest_list)
+
+
+
+
+    return render_template('dashboard.html', guest_list=guest_list)
 
 @app.route('/guest-list-entry', methods=['GET', 'POST'])
 @login_required
@@ -45,9 +56,34 @@ def guest_list_entry():
         them = check_login(friend_kerberos)[0]
         insert_into_connections(you, them)
 
-        return 'Guest successfully added'
+        return redirect(url_for('dashboard'))
 
     return render_template('guest_entry.html')
+
+@app.route('/edit-information', methods=['GET', 'POST'])
+@login_required
+def edit_information():
+
+    your_kerberos = session['kerberos']
+
+
+    if request.method == 'POST':
+        dorm = request.form['dorm']
+
+        update_dorm_info(your_kerberos, dorm)
+
+        return redirect(url_for('dashboard'))
+
+
+
+
+    you = fetch_user_by_kerb(your_kerberos)
+
+
+    dorm = you[5]
+
+    return render_template('edit_information.html', dorm=dorm)
+
 
 
 @app.route('/access', methods=['GET', 'POST'])
