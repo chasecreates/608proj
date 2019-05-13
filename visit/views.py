e32bf17877042372019bba1cf7fd09c0a33b58fb
chasebyinfinity <chasebyinfinity@gmail.com>
2019-05-13 18:29:30 -0400

    bam

--- visit/views.py
+++ visit/views.py
@@ -108,7 +108,7 @@ def access():
         if not requesting_student:
             return 'Not a valid student in the database'
         requesting_student_id = requesting_student[0]
-        # insert_into_attempts(studentID, requesting_student[1], requesting_student[2], requesting_student[3])
+        insert_into_attempts(studentID, requesting_student[1], requesting_student[2], requesting_student[3])
 
         conns = []
         get_conns_of_user(requesting_student_id, conns)
